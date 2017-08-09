efeb11d13536e44e94b3e486a2ac2ffd9b53128f
tingpalm <tingx44@hotmail.com>
2017-08-09 20:21:03 +0700

    Add files via upload

---
+++ ibcon.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>IBEACON</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+		<img src="img/header.png" />
+		<div id="phones">
+			<p>Detecting ibeacons...</p>
+		</div>
+
+        <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+    </body>
+</html>
